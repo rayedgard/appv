82b2cf2b69256c2df217cd23d2caa8eccac68ed4
rayedgard <rayedgard@gmail.com>
2015-06-27 18:03:53 -0500

    inicio de sistema

---
+++ sisventas.sql
@@ -0,0 +1,180 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.0.51b-community-nt-log : Database - sisventas
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`sisventas` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `sisventas`;
+
+/*Table structure for table `caracfisicas` */
+
+DROP TABLE IF EXISTS `caracfisicas`;
+
+CREATE TABLE `caracfisicas` (
+  `id` int(10) NOT NULL,
+  `dimencion` varchar(200) default NULL,
+  `peso` varchar(100) default NULL,
+  `color` char(30) default NULL,
+  `estado` int(1) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `caracfisicas` */
+
+LOCK TABLES `caracfisicas` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `categoria` */
+
+DROP TABLE IF EXISTS `categoria`;
+
+CREATE TABLE `categoria` (
+  `id` int(10) NOT NULL,
+  `nombre` varchar(200) default NULL,
+  `descripcion` text,
+  `idProveedor` int(10) default NULL,
+  `estado` int(1) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `categoria` */
+
+LOCK TABLES `categoria` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `cliente-proveedor` */
+
+DROP TABLE IF EXISTS `cliente-proveedor`;
+
+CREATE TABLE `cliente-proveedor` (
+  `documento` char(12) NOT NULL,
+  `razon` varchar(300) default NULL,
+  `telefono` char(20) default NULL,
+  `direccion` varchar(200) default NULL,
+  `tipo` int(1) default NULL,
+  PRIMARY KEY  (`documento`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `cliente-proveedor` */
+
+LOCK TABLES `cliente-proveedor` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `descuento` */
+
+DROP TABLE IF EXISTS `descuento`;
+
+CREATE TABLE `descuento` (
+  `id` int(10) NOT NULL,
+  `nombre` varchar(200) default NULL,
+  `porcentaje` int(11) default NULL,
+  `estado` int(1) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `descuento` */
+
+LOCK TABLES `descuento` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `producto` */
+
+DROP TABLE IF EXISTS `producto`;
+
+CREATE TABLE `producto` (
+  `id` int(10) NOT NULL auto_increment,
+  `nombre` varchar(200) default NULL,
+  `detalle` text,
+  `imagen` blob,
+  `precio` decimal(8,2) default NULL,
+  `idCategoria` int(11) default NULL,
+  `stock` int(10) default NULL,
+  `idDescuento` int(10) default NULL,
+  `idPromocion` int(10) default NULL,
+  `idUnidades` int(10) default NULL,
+  `idCaractFisicas` int(10) default NULL,
+  `estado` int(1) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `producto` */
+
+LOCK TABLES `producto` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `promocion` */
+
+DROP TABLE IF EXISTS `promocion`;
+
+CREATE TABLE `promocion` (
+  `id` int(10) NOT NULL auto_increment,
+  `nombre` varchar(200) default NULL,
+  `descripcion` text,
+  `estado` int(1) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `promocion` */
+
+LOCK TABLES `promocion` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `unidades` */
+
+DROP TABLE IF EXISTS `unidades`;
+
+CREATE TABLE `unidades` (
+  `id` int(10) NOT NULL,
+  `nombre` varchar(200) default NULL,
+  `abreviatura` char(10) default NULL,
+  `valorBase` int(10) default NULL,
+  `estado` int(1) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `unidades` */
+
+LOCK TABLES `unidades` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `ventas` */
+
+DROP TABLE IF EXISTS `ventas`;
+
+CREATE TABLE `ventas` (
+  `id` int(10) NOT NULL,
+  `idClinete` int(10) default NULL,
+  `subtotal` decimal(8,2) default NULL,
+  `impuesto` decimal(8,2) default NULL,
+  `total` decimal(8,2) default NULL,
+  `estado` int(1) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `ventas` */
+
+LOCK TABLES `ventas` WRITE;
+
+UNLOCK TABLES;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
