9012360fac8db7f99aa276c5d3f4ef76283909de
rayedgard <rayedgard@gmail.com>
2015-08-14 18:41:55 -0500

    cambios en frmVentas

--- appVentas/app/frmVenta.Designer.cs
+++ appVentas/app/frmVenta.Designer.cs
@@ -37,20 +37,33 @@
             this.label1 = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tbCantidad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gbResumen = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.gbResumen.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.gbResumen);
             this.panel2.Controls.Add(this.gbTiposTarjeta);
             this.panel2.Controls.Add(this.gbTipoComprobante);
             this.panel2.Controls.Add(this.gbFormasPago);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel2.Location = new System.Drawing.Point(773, 0);
+            this.panel2.Location = new System.Drawing.Point(984, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(224, 510);
+            this.panel2.Size = new System.Drawing.Size(251, 622);
             this.panel2.TabIndex = 1;
             // 
             // gbTiposTarjeta
@@ -58,7 +71,7 @@
             this.gbTiposTarjeta.Enabled = false;
             this.gbTiposTarjeta.Location = new System.Drawing.Point(6, 291);
             this.gbTiposTarjeta.Name = "gbTiposTarjeta";
-            this.gbTiposTarjeta.Size = new System.Drawing.Size(212, 127);
+            this.gbTiposTarjeta.Size = new System.Drawing.Size(233, 127);
             this.gbTiposTarjeta.TabIndex = 2;
             this.gbTiposTarjeta.TabStop = false;
             this.gbTiposTarjeta.Text = "Tipos de tarjeta";
@@ -67,7 +80,7 @@
             // 
             this.gbTipoComprobante.Location = new System.Drawing.Point(6, 12);
             this.gbTipoComprobante.Name = "gbTipoComprobante";
-            this.gbTipoComprobante.Size = new System.Drawing.Size(212, 129);
+            this.gbTipoComprobante.Size = new System.Drawing.Size(233, 129);
             this.gbTipoComprobante.TabIndex = 1;
             this.gbTipoComprobante.TabStop = false;
             this.gbTipoComprobante.Text = "Tipo de comprobante";
@@ -76,26 +89,32 @@
             // 
             this.gbFormasPago.Location = new System.Drawing.Point(6, 147);
             this.gbFormasPago.Name = "gbFormasPago";
-            this.gbFormasPago.Size = new System.Drawing.Size(212, 136);
+            this.gbFormasPago.Size = new System.Drawing.Size(233, 136);
             this.gbFormasPago.TabIndex = 0;
             this.gbFormasPago.TabStop = false;
             this.gbFormasPago.Text = "Formas de pago";
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.tbCantidad);
+            this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.tbBusca);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(773, 125);
+            this.panel1.Size = new System.Drawing.Size(984, 125);
             this.panel1.TabIndex = 2;
             // 
             // tbBusca
             // 
+            this.tbBusca.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.tbBusca.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
             this.tbBusca.Location = new System.Drawing.Point(75, 89);
             this.tbBusca.Name = "tbBusca";
-            this.tbBusca.Size = new System.Drawing.Size(664, 20);
+            this.tbBusca.Size = new System.Drawing.Size(724, 20);
             this.tbBusca.TabIndex = 1;
             // 
             // label1
@@ -110,9 +129,9 @@
             // panel3
             // 
             this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel3.Location = new System.Drawing.Point(0, 446);
+            this.panel3.Location = new System.Drawing.Point(0, 558);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(773, 64);
+            this.panel3.Size = new System.Drawing.Size(984, 64);
             this.panel3.TabIndex = 3;
             // 
             // dataGridView1
@@ -121,14 +140,145 @@
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(0, 125);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(773, 321);
+            this.dataGridView1.Size = new System.Drawing.Size(984, 433);
             this.dataGridView1.TabIndex = 4;
+            // 
+            // tbCantidad
+            // 
+            this.tbCantidad.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.tbCantidad.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.tbCantidad.Location = new System.Drawing.Point(881, 89);
+            this.tbCantidad.Name = "tbCantidad";
+            this.tbCantidad.Size = new System.Drawing.Size(74, 20);
+            this.tbCantidad.TabIndex = 3;
+            this.tbCantidad.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(820, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cantidad";
+            // 
+            // gbResumen
+            // 
+            this.gbResumen.Controls.Add(this.textBox3);
+            this.gbResumen.Controls.Add(this.textBox2);
+            this.gbResumen.Controls.Add(this.textBox1);
+            this.gbResumen.Controls.Add(this.label5);
+            this.gbResumen.Controls.Add(this.label4);
+            this.gbResumen.Controls.Add(this.label3);
+            this.gbResumen.Enabled = false;
+            this.gbResumen.Location = new System.Drawing.Point(6, 431);
+            this.gbResumen.Name = "gbResumen";
+            this.gbResumen.Size = new System.Drawing.Size(233, 179);
+            this.gbResumen.TabIndex = 3;
+            this.gbResumen.TabStop = false;
+            this.gbResumen.Text = "Resumen";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(4, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "I.G.V.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "SUB TOTAL";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(3, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "TOTAL";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.textBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(103, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(124, 45);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox2
+            // 
+            this.textBox2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.textBox2.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.textBox2.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.textBox2.Location = new System.Drawing.Point(103, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(124, 45);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "0";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox3
+            // 
+            this.textBox3.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.textBox3.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.textBox3.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.textBox3.Location = new System.Drawing.Point(103, 114);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(124, 45);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "0";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox4
+            // 
+            this.textBox4.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.textBox4.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.textBox4.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.textBox4.Location = new System.Drawing.Point(123, 12);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(124, 45);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "0";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(8, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "DESCUENTO";
             // 
             // frmVenta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(997, 510);
+            this.ClientSize = new System.Drawing.Size(1235, 622);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel1);
@@ -140,6 +290,8 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.gbResumen.ResumeLayout(false);
+            this.gbResumen.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -155,5 +307,16 @@
         private System.Windows.Forms.GroupBox gbTipoComprobante;
         private System.Windows.Forms.GroupBox gbFormasPago;
         private System.Windows.Forms.GroupBox gbTiposTarjeta;
+        private System.Windows.Forms.TextBox tbCantidad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox gbResumen;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
     }
 }
--- appVentas/app/frmVenta.cs
+++ appVentas/app/frmVenta.cs
@@ -18,6 +18,8 @@ namespace app
         object[] DatosCategoria = new object[4];
         string string_ArchivoConfiguracion;
 
+
+        //lista para capturar los datos
         List<string> lista = new List<string>();
 
 
@@ -37,12 +39,14 @@ namespace app
         private void listaDatos()
         {
             ConexionBD.Conectar(false, string_ArchivoConfiguracion);
-            DataSet datos = ConexionBD.EjecutarProcedimientoReturnDataSet("ventas_formas_pago");
             DataSet datos1 = ConexionBD.EjecutarProcedimientoReturnDataSet("ventas_tipos_comprobante");
+            DataSet datos = ConexionBD.EjecutarProcedimientoReturnDataSet("ventas_formas_pago");
             DataSet datos2 = ConexionBD.EjecutarProcedimientoReturnDataSet("ventas_tipos_tarjeta");
             
+
+            // para generar los radiobutons de FORMAS DE PAGO
             int y = 0;
-            foreach (DataRow row in datos.Tables[0].Rows)
+            foreach (DataRow row in datos1.Tables[0].Rows)
             {
                 RadioButton radio = new RadioButton();
                 radio.Text = Convert.ToString(row["nombre"]);
@@ -51,7 +55,7 @@ namespace app
                 y += 20;
                 radio.Location = new Point(20, y);
 
-                gbFormasPago.Controls.Add(radio);
+                gbTipoComprobante.Controls.Add(radio);
 
                 if (Convert.ToInt32(radio.Tag) == 1)
                 {
@@ -63,9 +67,9 @@ namespace app
                
             }
 
-
+            // para generar los radiobutons de COMPROBANTES DE PAGO
             int z = 0;
-            foreach (DataRow row1 in datos1.Tables[0].Rows)
+            foreach (DataRow row1 in datos.Tables[0].Rows)
             {
                 RadioButton radio1 = new RadioButton();
                 radio1.Text = Convert.ToString(row1["nombre"]);
@@ -74,17 +78,17 @@ namespace app
                 z += 20;
                 radio1.Location = new Point(20, z);
 
-                gbTipoComprobante.Controls.Add(radio1);
+                gbFormasPago.Controls.Add(radio1);
                 
                 if (Convert.ToInt32(radio1.Tag) == 1)
                 {
                     radio1.Checked = true;
                     lista.Add(Convert.ToString(row1["nombre"]));
                 }
-                radio1.Click += new EventHandler(radiobuton);
+                radio1.Click += new EventHandler(radiobutonTipoTarjeta);
             }
 
-            /*------PARA GENERAR RADIO BUTUNS------*/
+            // para generar los radiobutons de TIPOS DE TARJETA
             int zz = 0;
             foreach (DataRow row2 in datos2.Tables[0].Rows)
             {
@@ -109,26 +113,95 @@ namespace app
         }
         private void frmVenta_Load(object sender, EventArgs e)
         {
-            
+            ConexionBD.Conectar(false, string_ArchivoConfiguracion);
+
+            ConexionBD.autoCompletar(tbBusca, "SELECT nombre,nroserie FROM `producto`");
+            ConexionBD.Desconectar();
             
         }
 
 
-
+        /// <summary>
+        /// METODO PARA CAPTURAR EL EVENTO CLICK
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void radiobuton(object sender, EventArgs e)
         {
             var nombre = (RadioButton)sender;
            // MessageBox.Show(nombre.Text);
-            if(nombre.Text == "TARJETA")
+            if (nombre.Text == "TARJETA")
             {
-                gbTiposTarjeta.Enabled = true;
-              
+                tipo = Tipo.radio;
+            }
+            
+
+            if (nombre.Checked)
+            {
+                MessageBox.Show(nombre.Text);
+                
             }
 
-            if(nombre.Checked)
-                MessageBox.Show(nombre.Text);
+            habilitaBoton();
         }
+
+
+
+        /// <summary>
+        /// CAPTURA EVENTO CLICK PARA EL GRUPO DE FORMAS DE PAGO
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void radiobutonTipoTarjeta(object sender, EventArgs e)
+        {
+            var nombre = (RadioButton)sender;
+            if (nombre.Text == "TARJETA")
+            {
+                tipo = Tipo.radio;
+            }
+            else
+            {
+                tipo = Tipo.inicio;
+            }
+
+             habilitaBoton();
+        }
+
+
         
+
+
+
+
+
+
+        
+
+        #region ------para las logica de botoners-------------
+        private enum Tipo
+        {
+            guardar, modificar, eliminar, grid, radio,inicio
+        }
+        private Tipo tipo;
+
+
+
+
+        private void habilitaBoton()
+        {
+            //btnGuardar.Enabled = tipo == Tipo.guardar || tipo == Tipo.modificar;
+            //gbDatosPersonales.Enabled = tipo == Tipo.guardar || tipo == Tipo.modificar;
+            //gbDatosContacto.Enabled = tipo == Tipo.guardar || tipo == Tipo.modificar;
+            //dgvDatos.Enabled = dgvDatos.Rows.Count > 0;
+
+            gbTiposTarjeta.Enabled = tipo == Tipo.radio;
+
+        }
+        #endregion
+
+
+
+
 
 
 
